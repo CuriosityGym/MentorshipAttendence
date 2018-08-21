5eb1b4aac8690398c535df6c1118754c06228960
rupin <rupin.chheda@gmail.com>
2018-08-21 17:41:06 +0530

    IFTTT

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Response, Flask, current_app as app
 import os
+import requests
 app = Flask(__name__)
+IFTTT_KEY=os.environ.get("IFTTT_KEY")
+IFTTT_EVENT_NAME=os.environ.get("IFTTT_EVENT_NAME")
+IFTTT_URL="https://maker.ifttt.com/trigger/"+IFTTT_KEY+"/with/key/"+IFTTT_EVENT_NAME
+
+
 
 @app.route('/')
 def index():
@@ -9,8 +15,10 @@ def index():
 @app.route('/<cardID>/<locationID>',methods=['GET','POST'])
 def acceptSwipeRequest(cardID, locationID):
     try:
-        cardOwnerName=os.environ.get(cardID)+"Hello"
-        return cardOwnerName
+        cardOwnerName=os.environ.get(cardID)
+        dataURL=IFTTT_URL+ "?value1="+cardOwnerName+"&value2="+locationID+"&value3="
+        response=requests.get(IFTTT_URL)
+        return response.text
     except:
         return "Not available"
     #return "a"
