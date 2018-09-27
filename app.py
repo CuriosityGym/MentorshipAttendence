a6be193f3736373c2a48b8aee62f903a613fc050
rupin <rupin.chheda@gmail.com>
2018-09-27 15:43:21 +0530

    3D files and Unknown card

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import requests
 import sys 
 app = Flask(__name__)
 IFTTT_KEY=os.environ.get("IFTTT_KEY")
-IFTTT_EVENT_NAME=os.environ.get("IFTTT_EVENT_NAME")
+IFTTT_MENTORSHIP_EVENT_NAME=os.environ.get("IFTTT_EVENT_NAME")
+IFTTT_UNKNOWN_EVENT_NAME=os.environ.get("IFTTT_UNKNOWN_NAME")
 #IFTTT_EVENT_NAME="track_mentorship"
 #IFTTT_KEY="c1BVwMuv-fI8ryLpLIihJe"
-IFTTT_URL="https://maker.ifttt.com/trigger/"+IFTTT_EVENT_NAME+"/with/key/"+IFTTT_KEY
+IFTTT_MENTORSHIP_URL="https://maker.ifttt.com/trigger/"+IFTTT_MENTORSHIP_EVENT_NAME+"/with/key/"+IFTTT_KEY
+IFTTT_UNKNOWN_CARD_URL="https://maker.ifttt.com/trigger/"+IFTTT_UNKNOWN_EVENT_NAME+"/with/key/"+IFTTT_KEY
 
 
 
@@ -19,13 +21,24 @@ def index():
 def acceptSwipeRequest(cardID, locationID):
     try:
         cardOwnerName=os.environ.get(cardID)
-        data={}
-        data["value1"]=cardOwnerName
-        data["value2"]=locationID
-        data["value3"]=""        
-        #sys.stdout.write(IFTTT_URL)
-        response=requests.post(IFTTT_URL, data=data)
-        return response.text
+        if(cardOwnerName is not None):
+            
+            data={}
+            data["value1"]=cardOwnerName
+            data["value2"]=locationID
+            data["value3"]=""        
+            #sys.stdout.write(IFTTT_URL)
+            response=requests.post(IFTTT_MENTORSHIP_URL, data=data)
+            return response.text
+        else:
+            data={}
+            data["value1"]=cardID
+            data["value2"]=locationID
+            data["value3"]=""        
+            #sys.stdout.write(IFTTT_URL)
+            response=requests.post(IFTTT_UNKNOWN_CARD_URL, data=data)
+            return response.text
+        
     except:
         return "Error"
  
